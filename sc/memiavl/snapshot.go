1f7a20118930dd7059dc2e9e7a07d9880241a5df
yzang2019 <zymfrank@gmail.com>
2025-10-20 11:17:03 -0700

    Do not prefetch kvs

--- sc/memiavl/snapshot.go
+++ sc/memiavl/snapshot.go
@@ -623,13 +623,11 @@ func (snapshot *Snapshot) prefetchNodesAndLeaves(snapshotDir, treeName string) {
 	// If most pages are already resident, skip prefetch
 	residentNodes, errNodes := residentRatio(snapshot.nodes)
 	residentLeaves, errLeaves := residentRatio(snapshot.leaves)
-	residentKvs, errKv := residentRatio(snapshot.kvs)
 	threshold := 0.8
 	fmt.Printf("[PREFETCH] Tree %s nodes page cache residency ratio is %f\n", treeName, residentNodes)
 	fmt.Printf("[PREFETCH] Tree %s leaves page cache residency ratio is %f\n", treeName, residentLeaves)
-	fmt.Printf("[PREFETCH] Tree %s kvs page cache residency ratio is %f\n", treeName, residentKvs)
-	if errNodes == nil && errLeaves == nil && errKv == nil {
-		if residentNodes >= threshold && residentLeaves >= threshold && residentKvs > threshold {
+	if errNodes == nil && errLeaves == nil {
+		if residentNodes >= threshold && residentLeaves >= threshold {
 			fmt.Printf("[PREFETCH] Skipped prefetching for tree %s\n", treeName)
 			return
 		}
@@ -641,10 +639,6 @@ func (snapshot *Snapshot) prefetchNodesAndLeaves(snapshotDir, treeName string) {
 
 	if residentLeaves < threshold {
 		_ = SequentialReadAndFillPageCache(filepath.Join(snapshotDir, FileNameLeaves))
-	}
-
-	if residentKvs < threshold {
-		_ = SequentialReadAndFillPageCache(filepath.Join(snapshotDir, FileNameKVs))
 	}
 
 }
